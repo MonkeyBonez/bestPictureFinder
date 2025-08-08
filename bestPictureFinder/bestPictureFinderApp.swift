62704fce3aea9463e934245b51274d69363e6c02
Snehal Mulchandani <mulchandanisnehal1@gmail.com>
2025-08-07 17:58:21 -0700

    Initial Commit

---
+++ bestPictureFinder/bestPictureFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  bestPictureFinderApp.swift
+//  bestPictureFinder
+//
+//  Created by Snehal Mulchandani on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct bestPictureFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
